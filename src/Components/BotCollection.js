b2512cfa8674802a4105805c30a027b21db24ccd
gabriellapendo7 <gabriellapendo12@gmail.com>
2024-05-06 00:04:52 +0300

    final commit

---
+++ src/Components/BotCollection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BotCard from './BotCard';
+
+function BotCollection({ bots, addBotToArmy, deleteBot }) {
+  const mappedBots = bots.map((bot) => {
+    return <BotCard
+    bot={bot}
+    key={bot.id}
+    clickAdd={addBotToArmy}
+    clickDelete={deleteBot}/>;
+  });
+
+  return (
+    <div className='ui-four-column-grid'>
+      Collection of all bots
+      <div className='row'>{mappedBots}</div>
+    </div>
+  );
+}
+
+export default BotCollection;
